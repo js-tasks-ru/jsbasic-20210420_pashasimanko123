755c402d937407cbfb1853bdd1fc31755b12ece9
PoulWalcker <pashasimanko123@gmail.com>
2021-04-26 23:26:13 +0300

    refactor of  1st task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,6 +1,11 @@
 function factorial(n) {
-  if (n <= 1) {
+  if (n === 1) {
     return 1
   }
-  return n * factorial (n - 1)
+  // return n * factorial (n - 1)
+  const result = 0;
+  for(let i = n; i <= n; i--) {
+    result *= i;
+  }
+  return result
 }
