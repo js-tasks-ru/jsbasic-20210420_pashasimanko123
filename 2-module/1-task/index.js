242ba003a21167c0aba11be9da177cc006560a35
PoulWalcker <pashasimanko123@gmail.com>
2021-05-08 17:52:19 +0300

    1/2 module tasks

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+
+  for(let item in salaries) {
+    if(!isFinite(salaries[item])) continue;
+    sum += salaries[item]
+  }
+  return sum;
+
 }
